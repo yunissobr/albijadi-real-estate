08e9c72e4864e26c95d3cf84f7b7f16a19b0e319
yunissobr <unisoub@gmail.com>
2021-06-23 13:05:16 +0100

    add yaml configuration

--- app/config/config.php
+++ app/config/config.php
@@ -1,25 +1,19 @@
 <?php
-define('ROOT', dirname(dirname(dirname(__FILE__))));
-define('APPROOT', dirname(dirname(__FILE__)));
-define('SITENAME', 'ALBIJADI REAL ESTATE.');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'albijadi_real_estate');
-define('URLROOT', 'http://127.0.0.1/albijadi/');
-
-
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'yuniwirh_albijadi');
-// define('DB_PASS', '=&R[HlRkxX)Z');
-// define('DB_NAME', 'yuniwirh_albijadi');
 // define('ROOT', dirname(dirname(dirname(__FILE__))));
 // define('APPROOT', dirname(dirname(__FILE__)));
-// define('URLROOT', 'http://0502677779.com/');
 // define('SITENAME', 'ALBIJADI REAL ESTATE.');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'albijadi_real_estate');
+// define('URLROOT', 'http://127.0.0.1/albijadi/');
 
 
-
-
-
-  // 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'yuniwirh_albijadi');
+define('DB_PASS', '=&R[HlRkxX)Z');
+define('DB_NAME', 'yuniwirh_albijadi');
+define('ROOT', dirname(dirname(dirname(__FILE__))));
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://0502677779.com/');
+define('SITENAME', 'ALBIJADI REAL ESTATE.');
