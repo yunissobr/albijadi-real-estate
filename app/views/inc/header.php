26ca445b16877fc8f700013afa4a5cbf52017bb0
yunissobr <unisoub@gmail.com>
2021-06-23 14:02:11 +0100

    upload edits

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -35,7 +35,7 @@ $config = $configs->get();
                                     <i class="fa fa-instagram"></i>
                                 </a>
                                 <a
-                                    href="https://api.whatsapp.com/send?phone=920002313&text=Hello,%20I%20would%20like%20to%20know%20more%20informations?">
+                                    href="https://api.whatsapp.com/send?phone=0502677779&text=Hello,%20I%20would%20like%20to%20know%20more%20informations?">
                                     <i class="fa fa-whatsapp"></i>
                                 </a>
                             </div>
@@ -62,20 +62,20 @@ $config = $configs->get();
                                     <ul id="navigation">
                                         <li><a id="_homepage"
                                                 <?php echo $data['page'] == 'home' ? 'class="active"' : '' ?>
-                                                href="<?php  echo URLROOT ?>">home</a></li>
+                                                href="<?php echo URLROOT ?>">home</a></li>
                                         <li><a id="_discover"
                                                 <?php echo $data['page'] == 'search' || $data['page'] == 'single_property' ? 'class="active"' : '' ?>
-                                                href="<?php  echo URLROOT.'properties/search' ?>">Properties</a></li>
+                                                href="<?php echo URLROOT . 'properties/search' ?>">Properties</a></li>
                                         <li><a id="_aboutus"
                                                 <?php echo $data['page'] == 'about_us' ? 'class="active"' : '' ?>
-                                                href="<?php  echo URLROOT.'pages/about' ?>">About us</a></li>
+                                                href="<?php echo URLROOT . 'pages/about' ?>">About us</a></li>
                                         <li><a id="_services"
                                                 <?php echo $data['page'] == 'services' ? 'class="active"' : '' ?>
-                                                href="<?php  echo URLROOT.'pages/services#services' ?>">Services</a>
+                                                href="<?php echo URLROOT . 'pages/services#services' ?>">Services</a>
                                         </li>
                                         <li><a id="_contact"
                                                 <?php echo $data['page'] == 'contact' ? 'class="active"' : '' ?>
-                                                href="<?php  echo URLROOT.'pages/contact' ?>">Contact</a></li>
+                                                href="<?php echo URLROOT . 'pages/contact' ?>">Contact</a></li>
 
                                     </ul>
 
@@ -86,19 +86,22 @@ $config = $configs->get();
                             <div class="Appointment">
 
                                 <div class="book_btn d-none d-lg-block">
-                                    <a id="_addproperty"  href="<?php echo URLROOT.'properties/newProperty' ?>">Add Property</a>
+                                    <a id="_addproperty" href="<?php echo URLROOT . 'properties/newProperty' ?>">Add
+                                        Property</a>
 
                                 </div>
                             </div>
                         </div>
                         <div class="col-12">
-                            <li id="ln" style="color: white; position: absolute; background: teal; left: 0; top: 0px; width: 37px; text-align: center; padding: 4px;"></li>
+                            <li id="ln"
+                                style="color: white; position: absolute; background: teal; left: 0; top: 0px; width: 37px; text-align: center; padding: 4px;">
+                            </li>
                             <div class="mobile_menu d-block d-lg-none">
 
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
 
             </div>
         </div>
@@ -109,38 +112,6 @@ $config = $configs->get();
 
 
     <script>
-    // function setLanguage(lang) {
-    //     localStorage.setItem('language', lang);
-    // }
 
-
-
-    // $(document).ready(function() {
-    //     console.log('ready')
-    //     setLanguage('en')
-    //     var obj = JSON.parse(getLn())
-    //     console.log(obj)
-    //     // $('#div1').text(language.header_slogan);
-    // });
-
-    // var ln;
-    // function getLn(callback){
-    //     (localStorage.getItem('language') == null) ? setLanguage('en') : false;
-
-    //     console.log('ln = ' + localStorage.getItem('language'))
-    //     var xobj = new XMLHttpRequest();
-    //     xobj.overrideMimeType("application/json");
-    //     xobj.open('GET', '<?php //echo URLROOT.'languages/'?>'+localStorage.getItem('language')+'.json', true); // Replace 'my_data' with the path to your file
-    //     xobj.onreadystatechange = function() {
-    //         if (xobj.readyState == 4 && xobj.status == "200") {
-
-    //             console.log(xobj.responseText)
-    //             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-    //             callback(xobj.responseText);
-    //             // return xobj.responseText;
-    //         }
-    //     };
-    //     xobj.send(null);
-    // }
     </script>
 </header>
